ed9b40a6dd4771dfe7bb376f05b8f3fcecb7e0ea
Qin Jian <qinjian623@gmail.com>
2013-08-20 17:02:39 +0800

    install mew

---
+++ elpa/mew-20130814.1255/mew-pkg.el
@@ -0,0 +1,2 @@
+(define-package "mew" "20130814.1255" "Messaging in the Emacs World [github]" 'nil)
+
--- init.el
+++ init.el
@@ -232,6 +232,31 @@ If set/leave chinese-font-size to nil, it will follow english-font-size"
     (tool-bar-mode -1)
     (menu-bar-mode -1)
     (setq frame-title-format "%n%F/%b")))
+(defun mew-settings ()
+  (autoload 'mew "mew" nil t)
+  (autoload 'mew-send "mew" nil t)
+
+  ;; Optional setup (Read Mail menu for Emacs 21):
+  (if (boundp 'read-mail-command)
+      (setq read-mail-command 'mew))
+  
+  ;; Optional setup (e.g. C-xm for sending a message):
+  (autoload 'mew-user-agent-compose "mew" nil t)
+  (if (boundp 'mail-user-agent)
+      (setq mail-user-agent 'mew-user-agent))
+  (if (fboundp 'define-mail-user-agent)
+      (define-mail-user-agent
+        'mew-user-agent
+        'mew-user-agent-compose
+        'mew-draft-send-message
+        'mew-draft-kill
+        'mew-send-hook))
+  
+  ;回复
+  
+  (setq mew-refile-guess-control
+        '(mew-refile-guess-by-folder
+          mew-refile-guess-by-alist)))
 
 (defun calendar-setting ()
   (progn
