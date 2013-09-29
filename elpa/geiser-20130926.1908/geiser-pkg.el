bf3d4fd0798e6c8e6e3f29a9d7fa92ed94dd71a5
Qin Jian <qinjian623@gmail.com>
2013-09-29 09:12:39 +0800

    geiser and php-mode

---
+++ elpa/geiser-20130926.1908/geiser-pkg.el
@@ -0,0 +1,2 @@
+(define-package "geiser" "20130926.1908" "GNU Emacs and Scheme talk to each other [github]" 'nil)
+
---
+++ elpa/php-eldoc-20130328.1526/php-eldoc-pkg.el
@@ -0,0 +1,2 @@
+(define-package "php-eldoc" "20130328.1526" "eldoc backend for php [github]" 'nil)
+
