adf32584f097a4c3f33ad3c172738886f7e9bc8e
Qin Jian <qinjian623@gmail.com>
2014-05-13 16:05:51 +0800

    .

--- personal/qj-python.el
+++ personal/qj-python.el
@@ -1,6 +1,8 @@
-(package-initialize)
+
 (elpy-enable)
 ;;(add-hook 'python-mode-hook 'jedi:setup)
 ;;(setq jedi:complete-on-dot t)                 ; optional
 ;;(setq jedi:get-in-function-call-delay 500)
+
+(provide 'qj-python)
 
